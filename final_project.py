44bd1649090d9d03d37daca0591248a51c00bd5a
Uendel Rocha <uendelrocha@gmail.com>
2024-03-10 21:16:29 -0300

    Update final_project.py
    Fix github path

--- final_project.py
+++ final_project.py
@@ -59,7 +59,7 @@ def filtering(df_sp, sector_default_value, cap_default_value, option_sector, div
 
 @st.cache
 def read_data():
-  path_data = "initial_version\project\s&p500.csv"
+  path_data = "s&p500.csv"
   df_sp = pd.read_csv(path_data)
 
   return df_sp
@@ -99,7 +99,7 @@ if __name__ == "__main__":
   st.title("S&P500 Screener & Stock Analysis")
   st.sidebar.title("Search Criteria")
 
-  image = Image.open("initial_version\project\stock.jpeg")
+  image = Image.open("stock.jpeg")
   _, col_image_2, _ = st.columns([1,3,1])
   with col_image_2:
     st.image(image, caption="@austindistel")
